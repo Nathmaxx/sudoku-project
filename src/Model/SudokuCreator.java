b677573e6700242cef7f2608437a1948ffd4fb01
Søren Starck <soren.starck@gmail.com>
2025-01-20 14:24:27 +0100

    fix : not random grid issue

--- src/Model/SudokuCreator.java
+++ src/Model/SudokuCreator.java
@@ -22,6 +22,20 @@ public class SudokuCreator {
 
     private void fillSudoku() {
         System.out.println("Filling sudoku of size " + this.sudoku.getSize());
+        int size = this.sudoku.getSize();
+        int sqrtSize = (int) Math.sqrt(size);
+
+        // Place one instance of each possible number randomly on the grid
+        for (int num = 1; num <= size; num++) {
+            int row, col;
+            do {
+                row = (int) (Math.random() * size);
+                col = (int) (Math.random() * size);
+            } while (this.sudoku.get(row, col) != 0);
+            this.sudoku.set(row, col, num);
+        }
+
+        // Use the solver to fill the rest of the grid
         Solver solver = new Solver(this.sudoku);
         if (solver.solveSudoku(0, 0)) {
             System.out.println("Sudoku successfully created:");
