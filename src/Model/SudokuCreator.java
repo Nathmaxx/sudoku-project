d0d1f17b02718c2f5d89d392b6ba783077ea52e5
Søren Starck <soren.starck@gmail.com>
2025-01-21 10:30:30 +0100

    feat : added more sizes

--- src/Model/SudokuCreator.java
+++ src/Model/SudokuCreator.java
@@ -1,23 +1,36 @@
 package Model;
 
-public class SudokuCreator {
+import java.util.function.Consumer;
 
+public class SudokuCreator {
     private Sudoku sudoku;
+    private Consumer<Double> progressListener;
 
     public SudokuCreator(int size) {
         this.sudoku = new Sudoku(size);
-        this.fillSudoku();
-    }
-
-    public Sudoku getSudoku() {
-        return this.sudoku;
     }
 
     public Sudoku generateSudoku(int size, int percentage) {
         this.sudoku = new Sudoku(size);
         this.fillSudoku();
+        System.out.println("Removing " + percentage + "% of numbers from the sudoku");
         this.removeNumbers(percentage);
         return this.sudoku;
+    }
+
+    private void removeNumbers(int percentage) {
+        int size = this.sudoku.getSize();
+        int totalCells = size * size;
+        int cellsToRemove = (int) (totalCells * (percentage / 100.0));
+
+        while (cellsToRemove > 0) {
+            int row = (int) (Math.random() * size);
+            int col = (int) (Math.random() * size);
+            if (this.sudoku.get(row, col) != 0) {
+                this.sudoku.set(row, col, 0);
+                cellsToRemove--;
+            }
+        }
     }
 
     private void fillSudoku() {
@@ -25,17 +38,13 @@ public class SudokuCreator {
         int size = this.sudoku.getSize();
         int sqrtSize = (int) Math.sqrt(size);
 
-        // Place one instance of each possible number randomly on the grid
-        for (int num = 1; num <= size; num++) {
-            int row, col;
-            do {
-                row = (int) (Math.random() * size);
-                col = (int) (Math.random() * size);
-            } while (this.sudoku.get(row, col) != 0);
-            this.sudoku.set(row, col, num);
+        // Fill the diagonal sub-grids
+        for (int i = 0; i < size; i += sqrtSize) {
+            fillDiagonalSubGrid(i, i);
+            updateProgress((double) i / size);
         }
 
-        // Use the solver to fill the rest of the grid
+        // Solve the partially filled Sudoku
         Solver solver = new Solver(this.sudoku);
         if (solver.solveSudoku(0, 0)) {
             System.out.println("Sudoku successfully created:");
@@ -44,18 +53,46 @@ public class SudokuCreator {
         }
     }
 
-    public void removeNumbers(int percentage) {
+    private void fillDiagonalSubGrid(int row, int col) {
         int size = this.sudoku.getSize();
-        int cellsToRemove = (int) Math.round((size * size) * (percentage / 100.0));
-        for (int i = 0; i < cellsToRemove; i++) {
-            int row = (int) (Math.random() * size);
-            int col = (int) (Math.random() * size);
-            if (this.sudoku.get(row, col) != 0) {
-                this.sudoku.set(row, col, 0);
-            } else {
-                i--;
+        int sqrtSize = (int) Math.sqrt(size);
+        for (int i = 0; i < sqrtSize; i++) {
+            for (int j = 0; j < sqrtSize; j++) {
+                int num;
+                do {
+                    num = (int) (Math.random() * size) + 1;
+                } while (!isSafeToPlace(row + i, col + j, num));
+                this.sudoku.set(row + i, col + j, num);
             }
         }
     }
 
+    private boolean isSafeToPlace(int row, int col, int num) {
+        for (int x = 0; x < this.sudoku.getSize(); x++) {
+            if (this.sudoku.get(row, x) == num || this.sudoku.get(x, col) == num) {
+                return false;
+            }
+        }
+        int sqrtSize = (int) Math.sqrt(this.sudoku.getSize());
+        int startRow = row - row % sqrtSize;
+        int startCol = col - col % sqrtSize;
+        for (int i = 0; i < sqrtSize; i++) {
+            for (int j = 0; j < sqrtSize; j++) {
+                if (this.sudoku.get(startRow + i, startCol + j) == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void setProgressListener(Consumer<Double> progressListener) {
+        this.progressListener = progressListener;
+    }
+
+    private void updateProgress(double progress) {
+        if (progressListener != null) {
+            progressListener.accept(progress);
+        }
+    }
 }
--- src/View/SudokuApp.java
+++ src/View/SudokuApp.java
@@ -4,10 +4,12 @@ import Model.Sudoku;
 import Model.SudokuCreator;
 import Model.Solver;
 import javafx.application.Application;
+import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
@@ -18,6 +20,7 @@ public class SudokuApp extends Application {
     private GridPane sudokuGrid;
     private int gridSize = 9;
     private Sudoku currentSudoku;
+    private ProgressBar progressBar;
 
     @Override
     public void start(Stage primaryStage) {
@@ -25,7 +28,7 @@ public class SudokuApp extends Application {
         root.setPadding(new Insets(10));
 
         ComboBox<Integer> sizeComboBox = new ComboBox<>();
-        sizeComboBox.getItems().addAll(4, 9, 16);
+        sizeComboBox.getItems().addAll(4, 9, 16, 25, 36, 49);
         sizeComboBox.setValue(gridSize);
         sizeComboBox.setOnAction(_ -> gridSize = sizeComboBox.getValue());
 
@@ -33,53 +36,88 @@ public class SudokuApp extends Application {
         difficultyComboBox.getItems().addAll("Facile", "Moyen", "Difficile");
         difficultyComboBox.setValue("Moyen");
 
+        progressBar = new ProgressBar(0);
+        progressBar.setPrefWidth(300);
+        progressBar.setVisible(false); // Initially invisible
+
         Button createSudokuButton = new Button("Créer un Sudoku");
         createSudokuButton.setOnAction(_ -> {
             int percentage = getRemovalPercentage(difficultyComboBox.getValue());
-            SudokuCreator sudokuCreator = new SudokuCreator(gridSize);
-            currentSudoku = sudokuCreator.generateSudoku(gridSize, percentage);
-            createSudokuGrid(currentSudoku);
+            Task<Void> task = new Task<Void>() {
+                @Override
+                protected Void call() throws Exception {
+                    SudokuCreator sudokuCreator = new SudokuCreator(gridSize);
+                    sudokuCreator.setProgressListener(progress -> updateProgress(progress, 1.0));
+                    currentSudoku = sudokuCreator.generateSudoku(gridSize, percentage);
+                    return null;
+                }
+
+                @Override
+                protected void succeeded() {
+                    createSudokuGrid(currentSudoku);
+                    progressBar.setVisible(false); // Hide progress bar when done
+                }
+
+                @Override
+                protected void running() {
+                    progressBar.setVisible(true); // Show progress bar when running
+                }
+            };
+
+            progressBar.progressProperty().bind(task.progressProperty());
+            new Thread(task).start();
         });
 
-        Button resolveSudokuButton = new Button("Résoudre un Sudoku");
-        resolveSudokuButton.setOnAction(_ -> solveSudoku());
+        Button solveSudokuButton = new Button("Résoudre");
+        solveSudokuButton.setOnAction(_ -> {
+            if (currentSudoku != null) {
+                Solver solver = new Solver(currentSudoku);
+                if (solver.solveSudoku(0, 0)) {
+                    createSudokuGrid(currentSudoku);
+                } else {
+                    System.out.println("Impossible de résoudre le Sudoku.");
+                }
+            }
+        });
 
         sudokuGrid = new GridPane();
-        createSudokuGrid(null);
+        sudokuGrid.setPadding(new Insets(10));
+        sudokuGrid.setHgap(1);
+        sudokuGrid.setVgap(1);
 
-        root.getChildren().addAll(sizeComboBox, difficultyComboBox, createSudokuButton, resolveSudokuButton,
-                sudokuGrid);
+        root.getChildren().addAll(sizeComboBox, difficultyComboBox, createSudokuButton, solveSudokuButton, progressBar, sudokuGrid);
 
-        Scene scene = new Scene(root, 800, 600);
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Sudoku Solver");
+        primaryStage.setTitle("Sudoku Generator");
+        primaryStage.sizeToScene(); // Adjust window size to fit content
         primaryStage.show();
     }
 
     private int getRemovalPercentage(String difficulty) {
         switch (difficulty) {
             case "Facile":
-                return 30;
+                return 40;
             case "Moyen":
-                return 50;
+                return 60;
             case "Difficile":
-                return 70;
+                return 80;
             default:
-                return 50;
+                return 60;
         }
     }
 
     private void createSudokuGrid(Sudoku sudoku) {
         sudokuGrid.getChildren().clear();
-        sudokuGrid.setGridLinesVisible(true);
-
-        for (int row = 0; row < gridSize; row++) {
-            for (int col = 0; col < gridSize; col++) {
+        int size = sudoku.getSize();
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
                 TextField cell = new TextField();
-                cell.setPrefWidth(50);
-                cell.setPrefHeight(50);
-                if (sudoku != null && sudoku.get(row, col) != 0) {
-                    cell.setText(String.valueOf(sudoku.get(row, col)));
+                cell.setPrefWidth(30);
+                cell.setPrefHeight(30);
+                int value = sudoku.get(row, col);
+                if (value != 0) {
+                    cell.setText(String.valueOf(value));
                     cell.setEditable(false);
                 }
                 sudokuGrid.add(cell, col, row);
@@ -87,18 +125,7 @@ public class SudokuApp extends Application {
         }
     }
 
-    private void solveSudoku() {
-        if (currentSudoku != null) {
-            Solver solver = new Solver(currentSudoku);
-            if (solver.solveSudoku(0, 0)) {
-                createSudokuGrid(currentSudoku);
-                System.out.println("Sudoku solved successfully.");
-            } else {
-                System.out.println("Failed to solve Sudoku.");
-            }
-        } else {
-            System.out.println("No Sudoku puzzle to solve.");
-        }
+    public static void main(String[] args) {
+        launch(args);
     }
-
 }
