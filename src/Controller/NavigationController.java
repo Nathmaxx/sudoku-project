69be14e0365e0694096e90af3aea738ebdb4e027
Nath <nathan.girardi@proton.me>
2025-02-09 20:15:39 +0100

    Javadoc NavigationController

--- src/Controller/NavigationController.java
+++ src/Controller/NavigationController.java
@@ -3,14 +3,29 @@ package Controller;
 
 import utils.ViewManager;
 
+/**
+ * Contrôleur pour gérer la navigation entre les différentes vues de
+ * l'application.
+ */
 public class NavigationController {
 
+    /** Gestionnaire de vues pour afficher les différentes pages */
     private ViewManager vm;
 
+    /**
+     * Constructeur pour initialiser le contrôleur avec le gestionnaire de vues.
+     *
+     * @param vm le gestionnaire de vues
+     */
     public NavigationController(ViewManager vm) {
         this.vm = vm;
     }
 
+    /**
+     * Navigue vers la page spécifiée.
+     *
+     * @param pageName le nom de la page à afficher
+     */
     public void navigation(String pageName) {
         vm.showView(pageName);
     }
