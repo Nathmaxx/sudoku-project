4c5e75bb4e967c9dfa350c8deecdd93437ca1ce5
Nath <nathan.girardi@proton.me>
2025-02-09 20:59:57 +0100

    Javadoc SizeCombobox

--- src/View/Components/SizeComboBox.java
+++ src/View/Components/SizeComboBox.java
@@ -2,8 +2,17 @@ package View.Components;
 
 import javafx.scene.control.ComboBox;
 
+/**
+ * Classe représentant une ComboBox pour sélectionner la taille de la grille de
+ * Sudoku.
+ */
 public class SizeComboBox extends ComboBox<Integer> {
 
+    /**
+     * Constructeur pour initialiser la ComboBox avec une valeur initiale.
+     *
+     * @param initialValue la valeur initiale de la ComboBox
+     */
     public SizeComboBox(int initialValue) {
         super();
         getItems().addAll(4, 9, 16);
