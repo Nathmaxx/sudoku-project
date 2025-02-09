db3be8431029c75b449dc5e78e6162d1ea72f3ff
Nath <nathan.girardi@proton.me>
2025-02-09 19:26:56 +0100

    Javadoc Multidoku

--- src/Model/Multidoku.java
+++ src/Model/Multidoku.java
@@ -1,17 +1,39 @@
 package Model;
 
+/**
+ * Classe qui gère plusieurs Sudokus partageant des cases communes.
+ * Cette classe permet de gérer un ensemble de Sudokus interconnectés.
+ */
 public class Multidoku {
 
+    /** Tableau des Sudokus interconnectés */
     public SharedSudoku[] sudokus;
 
+    /**
+     * Constructeur qui initialise un Multidoku avec un tableau de Sudokus.
+     *
+     * @param sudokus tableau de SharedSudoku à interconnecter
+     */
     public Multidoku(SharedSudoku[] sudokus) {
         this.sudokus = sudokus;
     }
 
+    /**
+     * Retourne le tableau des Sudokus interconnectés.
+     *
+     * @return tableau des SharedSudoku
+     */
     public SharedSudoku[] getSudokus() {
         return this.sudokus;
     }
 
+    /**
+     * Ajoute un nouveau Sudoku au Multidoku.
+     * Crée un nouveau tableau plus grand et y copie les Sudokus existants
+     * avant d'ajouter le nouveau.
+     *
+     * @param sudoku le SharedSudoku à ajouter
+     */
     public void addSudoku(SharedSudoku sudoku) {
         SharedSudoku[] newSudokus = new SharedSudoku[this.sudokus.length + 1];
         for (int i = 0; i < this.sudokus.length; i++) {
