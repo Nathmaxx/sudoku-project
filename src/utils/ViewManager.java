4a4bd8cd5a5b1a0a32278bd31844277f43741edd
Nath <nathan.girardi@proton.me>
2025-02-09 20:18:36 +0100

    Javadoc ViewManager

--- src/utils/ViewManager.java
+++ src/utils/ViewManager.java
@@ -7,19 +7,44 @@ import javafx.stage.Stage;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Classe responsable de la gestion des vues dans l'application.
+ * Permet de basculer entre différentes scènes.
+ */
 public class ViewManager {
+
+    /** Map associant les noms des vues aux scènes correspondantes */
     private Map<String, Scene> scenes = new HashMap<>();
+
+    /** Stage principal de l'application */
     private Stage primaryStage;
 
+    /**
+     * Constructeur pour initialiser le gestionnaire de vues avec le stage
+     * principal.
+     *
+     * @param primaryStage le stage principal de l'application
+     */
     public ViewManager(Stage primaryStage) {
         this.primaryStage = primaryStage;
     }
 
+    /**
+     * Ajoute une vue au gestionnaire de vues.
+     *
+     * @param name le nom de la vue
+     * @param view le Pane représentant la vue
+     */
     public void addView(String name, Pane view) {
         Scene scene = new Scene(view, 700, 700);
         scenes.put(name, scene);
     }
 
+    /**
+     * Affiche la vue correspondant au nom donné.
+     *
+     * @param name le nom de la vue à afficher
+     */
     public void showView(String name) {
         Scene scene = scenes.get(name);
         if (scene != null) {
