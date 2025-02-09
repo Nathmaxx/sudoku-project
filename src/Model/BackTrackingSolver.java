30abe128910c0a8288b85c90f9da2bcb815557c8
Søren Starck <soren.starck@gmail.com>
2025-02-09 19:03:41 +0100

    feat : logging solutions

--- src/Model/BackTrackingSolver.java
+++ src/Model/BackTrackingSolver.java
@@ -1,6 +1,10 @@
 package Model;
 
+import java.util.logging.Logger;
+
 public class BackTrackingSolver extends SudokuSolver {
+
+    private static final Logger logger = Logger.getLogger(BackTrackingSolver.class.getName());
 
     public BackTrackingSolver() {
         super();
@@ -13,11 +17,13 @@ public class BackTrackingSolver extends SudokuSolver {
                     for (int numberToTry = 1; numberToTry <= size; numberToTry++) {
                         if (isValidPlacement(numberToTry, column, row)) {
                             board[row][column] = numberToTry;
+                            logger.info("Placed " + numberToTry + " at (" + row + ", " + column + ")");
 
                             if (backTrackingSolve()) {
                                 return true;
                             } else {
                                 board[row][column] = 0;
+                                logger.info("Removed " + numberToTry + " from (" + row + ", " + column + ")");
                             }
                         }
                     }
@@ -27,5 +33,4 @@ public class BackTrackingSolver extends SudokuSolver {
         }
         return true;
     }
-
 }
--- src/Model/SudokuSolver.java
+++ src/Model/SudokuSolver.java
@@ -1,24 +1,28 @@
 package Model;
 
+import java.util.logging.Logger;
+
 public class SudokuSolver {
 
     protected int[][] board;
     protected int size;
     protected int boxSize;
+    private static final Logger logger = Logger.getLogger(SudokuSolver.class.getName());
 
     public SudokuSolver() {
-
     }
 
     public void setSudoku(Sudoku sudoku) {
         this.board = sudoku.getBoard();
         this.size = sudoku.getSize();
         this.boxSize = (int) Math.sqrt(size);
+        logger.info("Sudoku set with size " + size);
     }
 
     protected boolean isNumberInRow(int number, int row) {
         for (int i = 0; i < size; i++) {
             if (board[row][i] == number) {
+                logger.info("Number " + number + " found in row " + row);
                 return true;
             }
         }
@@ -28,6 +32,7 @@ public class SudokuSolver {
     protected boolean isNumberInColumn(int number, int col) {
         for (int i = 0; i < size; i++) {
             if (board[i][col] == number) {
+                logger.info("Number " + number + " found in column " + col);
                 return true;
             }
         }
@@ -41,6 +46,7 @@ public class SudokuSolver {
         for (int i = boxRow; i < boxRow + boxSize; i++) {
             for (int j = boxColumn; j < boxColumn + boxSize; j++) {
                 if (board[i][j] == number) {
+                    logger.info("Number " + number + " found in box at (" + row + ", " + column + ")");
                     return true;
                 }
             }
@@ -49,9 +55,11 @@ public class SudokuSolver {
     }
 
     protected boolean isValidPlacement(int number, int column, int row) {
-        return !isNumberInRow(number, row) &&
-                !isNumberInColumn(number, column) &&
-                !isNumberInBox(number, row, column);
+        boolean valid = !isNumberInRow(number, row) &&
+                        !isNumberInColumn(number, column) &&
+                        !isNumberInBox(number, row, column);
+        logger.info("Placement of number " + number + " at (" + row + ", " + column + ") is " + (valid ? "valid" : "invalid"));
+        return valid;
     }
 
     public int[][] getBoard() {
