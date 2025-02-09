1de12b9d020fa51dede3f555b4b100703acce71f
Nath <nathan.girardi@proton.me>
2025-02-09 20:51:07 +0100

    Generation

--- src/View/Generation.java
+++ src/View/Generation.java
@@ -15,29 +15,51 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import utils.ViewManager;
 
+/**
+ * Classe représentant la vue de génération de Sudoku.
+ * Gère l'affichage des options de génération et la grille de Sudoku.
+ */
 public class Generation extends BaseView {
 
+    /** Contrôleur pour gérer les interactions avec la vue de génération */
     private NavigationController navigationController;
+
+    /** COntrôleur pour gérer les intéracitons avec le sudoku */
     private SudokuController sudokuController;
 
+    /** Taille de la grille */
     private int gridSize = 9;
 
+    /** Sudoku actuellement affiché */
     private Sudoku currentSudoku;
 
+    /** ComboBox des trois difficultés */
     private ComboBox<String> difficultyComboBox;
 
+    /** Grille de Sudoku affichée dans la vue */
     private SudokuGrid sudokuGrid;
 
+    /** Bouton pour résoudre le Sudoku */
     private Button solveSudokuButton;
 
+    /** Barre de progression */
     private ProgressBar progressBar;
 
+    /**
+     * Constructeur pour initialiser la vue de génération de Sudoku.
+     *
+     * @param vm le gestionnaire de vues
+     */
     public Generation(ViewManager viewManager) {
         this.navigationController = new NavigationController(viewManager);
         this.sudokuController = new SudokuController(this);
         initializeUI();
     }
 
+    /**
+     * Initialise l'interface utilisateur de la vue de génération de Sudoku.
+     * Configure les composants et les ajoute à la scène.
+     */
     @Override
     protected void initializeUI() {
 
@@ -99,31 +121,65 @@ public class Generation extends BaseView {
                 progressBar);
     }
 
+    /**
+     * Affiche le Sudoku dans la grille.
+     *
+     * @param sudoku le Sudoku à afficher
+     */
     public void displaySudoku(Sudoku sudoku) {
         sudokuGrid.setSudoku(sudoku);
         sudokuGrid.displaySudoku();
     }
 
+    /**
+     * Affiche les boutons de résolution.
+     */
     public void showSolveButtons() {
         solveSudokuButton.setVisible(true);
     }
 
+    /**
+     * Retourne le niveau de difficulté sélectionné.
+     *
+     * @return le niveau de difficulté sélectionné
+     */
     public String getDifficulty() {
         return difficultyComboBox.getValue();
     }
 
+    /**
+     * Retourne la taille de la grille de Sudoku.
+     *
+     * @return la taille de la grille de Sudoku
+     */
     public int getGridSize() {
         return this.gridSize;
     }
 
+    /**
+     * Retourne le Sudoku actuellement affiché.
+     *
+     * @return le Sudoku actuellement affiché
+     */
     public Sudoku getCurrentSudoku() {
         return this.currentSudoku;
     }
 
+    /**
+     * Définit le Sudoku à afficher dans la vue.
+     *
+     * @param newSudoku le nouveau Sudoku à afficher
+     */
     public void setCurrentSudoku(Sudoku newSudoku) {
         this.currentSudoku = newSudoku;
     }
 
+    /**
+     * Retourne la barre de progression utilisée pour afficher l'avancement de la
+     * génération.
+     *
+     * @return la barre de progression
+     */
     public ProgressBar getProgressBar() {
         return this.progressBar;
     }
