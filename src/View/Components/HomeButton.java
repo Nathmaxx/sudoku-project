51e1bec5a9bad4e6a2f4c46cc29bcff9d8d72715
Nath <nathan.girardi@proton.me>
2025-02-09 21:00:46 +0100

    HomeButton

--- src/View/Components/HomeButton.java
+++ src/View/Components/HomeButton.java
@@ -3,8 +3,17 @@ package View.Components;
 import Controller.NavigationController;
 import javafx.scene.control.Button;
 
+/**
+ * Classe représentant un bouton pour retourner à l'accueil.
+ */
 public class HomeButton extends Button {
 
+    /**
+     * Constructeur pour initialiser le bouton avec une action de navigation.
+     *
+     * @param navigationController le contrôleur de navigation pour gérer le
+     *                             changement de vue
+     */
     public HomeButton(NavigationController navigationController) {
         super("Retour à l'accueil");
         this.setOnAction(event -> navigationController.navigation("accueil"));
